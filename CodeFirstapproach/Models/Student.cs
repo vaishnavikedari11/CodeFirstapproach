4bdc26584ebcb60075c91ea01d4278dfd51dffd6
Vaishnavi Kedari <Vaishnavi Kedari@DESKTOP-BHPPGIS>
2024-08-05 11:28:34 +0530

    Add project files.

---
+++ CodeFirstapproach/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CodeFirstapproach.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+        [Column("StudentName",TypeName ="varchar(100)")]
+        public string Name { get; set; }
+        [Column("StudentGender", TypeName = "varchar(20)")]
+        public string Gender { get; set; }
+        public string Age { get; set; }
+
+
+    }
+}
